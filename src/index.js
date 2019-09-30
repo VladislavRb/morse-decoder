96bd63452d733cd4626b0f88b5f58cabf57996df
VladislavRb <vlad.raba@mail.ru>
2019-09-30 22:49:56 +0300

    fix: do task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    function decodeSymbolInMorse(twoChars){
+        switch(twoChars){
+            case "10":
+                return ".";
+            case "11":
+                return "-";
+            default:
+                return "";
+        }
+    }
+
+    let letters = [];
+    for(let i = 0; i < expr.length; i += 10){
+        let letterInMorse = "";
+        for(let j = i; j < i + 10; j += 2){
+            letterInMorse += decodeSymbolInMorse(expr[j] + expr[j + 1]);
+        }
+        letters.push(letterInMorse);
+    }
+
+    let decodedString = letters.map(x => x in MORSE_TABLE ? MORSE_TABLE[x] : " ").join("");
+    return decodedString;
 }
 
 module.exports = {
